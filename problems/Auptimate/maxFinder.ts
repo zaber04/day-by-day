b7a1cbd2e53e31c4997a0e2bc2ec022204eff407
zaber mahbub <zabermahbub95@gmail.com>
2023-10-23 02:36:09 +0600

    store

---
+++ problems/Auptimate/maxFinder.ts
@@ -0,0 +1,16 @@
+/**
+ * Imagine you are given a
+dataset containing transactional records of different investors in various
+syndicates over several years. Each record contains an investor ID, syndicate
+ID, transaction amount, and transaction date. Your task is to identify and list
+the top 5 investors who have invested in the highest number of unique
+syndicates, along with the total amount they have invested.
+ */
+
+/**
+ * data = [
+ * {
+ * invId: '123', synId: 'abc', amt: 456.78, date: '20230517', uid: 
+ * }
+ * ];
+ */
